579a15d4f99ea6a1f42ad0c0c6ea9b868c4fc4fb
FlodexDotXyz <emzanyuda@gmail.com>
2026-06-25 10:38:59 +0700

    Create allocation.ts

---
+++ allocation.ts
@@ -0,0 +1,20 @@
+export function calculateAllocation() {
+
+  return [
+    {
+      token: "PUMP",
+      weight: 40
+    },
+
+    {
+      token: "BONK",
+      weight: 35
+    },
+
+    {
+      token: "DOGE",
+      weight: 25
+    }
+  ];
+
+}
